a293bb2449822ab31f8e1309fcfbf4ec99c89c31
Herman White <phoenixquint@gmail.com>
2018-10-27 23:19:56 -0400

    Updates

---
+++ precache-manifest.675b4f87b58021930d044b8f2eda1db1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "74f199f9304de10730c305368f9dc83f",
+    "url": "/butterscotch-taco/static/media/placeholder.74f199f9.jpg"
+  },
+  {
+    "revision": "3d7b5c3a4dee197d801a",
+    "url": "/butterscotch-taco/static/js/runtime~main.3d7b5c3a.js"
+  },
+  {
+    "revision": "afc7cbabbc46008d1422",
+    "url": "/butterscotch-taco/static/js/main.afc7cbab.chunk.js"
+  },
+  {
+    "revision": "482768dc30888bd9dafb",
+    "url": "/butterscotch-taco/static/js/1.482768dc.chunk.js"
+  },
+  {
+    "revision": "afc7cbabbc46008d1422",
+    "url": "/butterscotch-taco/static/css/main.6d7e42ec.chunk.css"
+  },
+  {
+    "revision": "482768dc30888bd9dafb",
+    "url": "/butterscotch-taco/static/css/1.cd62d6e6.chunk.css"
+  },
+  {
+    "revision": "a04f2d0cbf6334a3f5318efe2c7b6f3d",
+    "url": "/butterscotch-taco/index.html"
+  }
+];
